e968e7808bbd74c81359f6c741a7b39247e11e30
MarkNovik <novikmark@gmail.com>
2023-12-01 22:24:59 +0200

    Day 1 optimized

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -5,27 +5,28 @@ import kotlinx.io.readString
 import kotlin.time.measureTimedValue
 
 fun main() {
-    run(Day1)
-}
-
-fun run(day: AOC) {
-    print("Day ${day.day}: ")
-    runCatching { SystemFileSystem.source(Path("input/day${day.day}.txt")).buffered().readString() }.fold(
-        onSuccess = { input ->
-            print("\n\tPart1: ")
-            val (res1, time1) = measureTimedValue { day.part1(input) }
-            println("$res1, $time1")
-            print("\tPart2: ")
-            val (res2, time2) = measureTimedValue { day.part2(input) }
-            println("$res2, $time2")
-        },
-        onFailure = { err ->
-            println("Error while reading input: $err")
-        }
-    )
+    Day1.run()
+    Day1.run()
 }
 
 sealed class AOC(val day: Int) {
-    abstract fun part1(input: String): Any
-    abstract fun part2(input: String): Any
+    abstract fun part1(input: String): String
+    abstract fun part2(input: String): String
+
+    fun run() {
+        print("Day $day: ")
+        runCatching { SystemFileSystem.source(Path("input/day$day.txt")).buffered().readString() }.fold(
+            onSuccess = { input ->
+                print("\n\tPart1: ")
+                val (res1, time1) = measureTimedValue { part1(input) }
+                println("$res1, $time1")
+                print("\tPart2: ")
+                val (res2, time2) = measureTimedValue { part2(input) }
+                println("$res2, $time2")
+            },
+            onFailure = { err ->
+                println("Error while reading input: $err")
+            }
+        )
+    }
 }
