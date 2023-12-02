c494810dfbeabcb855b84eae21bd1cd972f472d9
MarkNovik <novikmark@gmail.com>
2023-12-02 14:19:31 +0200

    Day 2

--- src/commonMain/kotlin/Day2.kt
+++ src/commonMain/kotlin/Day2.kt
@@ -1,21 +1,17 @@
 import Day2.Cubes.*
 
 data object Day2 : AOC(2) {
-    override fun part1(input: String): String {
-        val redCap = Red(12)
-        val greenCap = Green(13)
-        val blueCap = Blue(14)
-        
-        return input.lineSequence().map(Game::parse).filter { game ->
+    override fun part1(input: String): String =
+        input.lineSequence().map(Game::parse).filter { game ->
             game.bundles.all {
                 when(it) {
-                    is Blue -> it.amount <= blueCap.amount
-                    is Green -> it.amount <= greenCap.amount
-                    is Red -> it.amount <= redCap.amount
+                    is Blue -> it.amount <= 12
+                    is Green -> it.amount <= 13
+                    is Red -> it.amount <= 14
                 }
             }
         }.sumOf(Game::id).toString()
-    }
+
     override fun part2(input: String): String =
         input.lineSequence().map(Game::parse).sumOf { game ->
             val red = game.bundles.filterIsInstance<Red>().maxOf(Cubes::amount)
