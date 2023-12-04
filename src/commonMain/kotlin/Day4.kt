84b5ce0ca15658efd44997d2d4c1ad47c5da2ea7
MarkNovik <novikmark@gmail.com>
2023-12-04 10:43:06 +0200

    Day 4

---
+++ src/commonMain/kotlin/Day4.kt
@@ -0,0 +1,33 @@
+data object Day4 : AOC(4) {
+    override fun part1(input: String): String = input
+        .lines()
+        .sumOf { line ->
+            1 shl (card(line) - 1) and Int.MAX_VALUE
+        }
+        .toString()
+
+    override fun part2(input: String): String = input
+        .lines()
+        .map(::card)
+        .let(::countFinalCards)
+        .toString()
+
+    private fun card(line: String): Int =
+        line.substringAfter(':').split('|').map(::numbers).let { (a, b) -> matches(a, b) }
+
+    private fun numbers(s: String) =
+        s.trim().split(' ').filterNot(String::isBlank).map(String::toInt)
+
+    private fun matches(winningNumbers: Collection<Int>, cardNumbers: Collection<Int>) =
+        (winningNumbers intersect cardNumbers.toSet()).size
+
+    private fun countFinalCards(cards: List<Int>): Int {
+        val memory = IntArray(cards.size) { 1 }
+        cards.forEachIndexed { id, matches ->
+            for (offset in 1..matches) {
+                memory[id + offset] += memory[id]
+            }
+        }
+        return memory.sum()
+    }
+}
--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -8,6 +8,7 @@ fun main() {
     Day1.run()
     Day2.run()
     Day3.run()
+    Day4.run()
 }
 
 sealed class AOC(private val day: Int) {
