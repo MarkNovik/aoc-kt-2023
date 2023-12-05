914f5b91aa869b844b84c3c767a152db379a698a
MarkNovik <novikmark@gmail.com>
2023-12-05 17:08:51 +0200

    Day 5 part 1

---
+++ src/commonMain/kotlin/Day5.kt
@@ -0,0 +1,61 @@
+data object Day5 : AOC(5) {
+    override fun part1(input: String): String {
+        val seeds = seeds(input)
+        val almanac = Almanac.parse(input)
+        return seeds.minOf(almanac::locateSeed).toString()
+    }
+
+    override fun part2(input: String): String = "TODO"
+
+    private val order = listOf(
+        "seed-to-soil",
+        "soil-to-fertilizer",
+        "fertilizer-to-water",
+        "water-to-light",
+        "light-to-temperature",
+        "temperature-to-humidity",
+        "humidity-to-location"
+    )
+
+    private fun seeds(input: String): List<Long> = input
+        .substringAfter("seeds: ")
+        .substringBefore('\n')
+        .split(" ").map(String::toLong)
+
+    //private fun seedRanges(input: String): List<SeedRange> = seeds(input).chunked(2) { SeedRange(it[0], it[1]) }
+    //class SeedRange(val start: Long, val length: Long)
+
+    class Range(private val source: Long, private val destination: Long, private val length: Long) {
+        fun map(item: Long): Long? =
+            if (item >= source && item < (source + length)) {
+                destination - (source - item)
+            } else {
+                null
+            }
+    }
+
+    class Almanac(
+        private val sections: List<AlmanacSection>
+    ) {
+        fun locateSeed(seed: Long) =
+            sections.fold(seed) { acc, section -> section.map(acc) }
+
+        companion object {
+            fun parse(input: String): Almanac = Almanac(order.map { section ->
+                AlmanacSection(input.substringAfter("$section map:\n")
+                    .substringBefore("\n\n")
+                    .lines().map {
+                        val (dest, src, len) = it.split(" ").map(String::toLong)
+                        Range(src, dest, len)
+                    })
+            })
+        }
+    }
+
+    class AlmanacSection(
+        private val ranges: List<Range>
+    ) {
+        fun map(item: Long): Long =
+            ranges.firstNotNullOfOrNull { it.map(item) } ?: item
+    }
+}
---
+++ src/commonTest/kotlin/Day5Test.kt
@@ -0,0 +1,49 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day5Test {
+
+    @Test
+    fun part1() {
+        assertEquals("35", Day5.part1(input))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals("46", Day5.part2(input))
+    }
+
+    val input = """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"""
+}
